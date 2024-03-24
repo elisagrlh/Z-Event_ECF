02e626a6b455a6afcf135aa31f7849507156e14d
Elisa Gerlach <elisagerlach17@gmail.com>
2024-03-24 14:41:27 +0100

    making site work again with django

--- SiteWeb/ZEvent/ZEvent/business/views.py
+++ SiteWeb/ZEvent/ZEvent/business/views.py
@@ -2,9 +2,33 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+#from django.contrib.auth.views import LoginView
+from django.views.generic import TemplateView
+
 
 
 def index(request):
-    f = open("C:/xampp/htdocs/ECF/SiteWeb/ZEvent/ZEvent/business/index.html", "r")
+    #f = open("C:/xampp/htdocs/ECF/SiteWeb/ZEvent/ZEvent/business/templates/business/index.html", "r")
+    #resp = f.read()
+    #template = loader.get_template("business/index.html")
+    #return HttpResponse(f"{resp}")
+    #return HttpResponse(template.render(request=request))
+    return render(request, "business/index.html")
+
+#def connexion(request):
+    #f = open("C:/xampp/htdocs/ECF/SiteWeb/ZEvent/ZEvent/business/templates/business/connexion.html", "r")
+    #resp = f.read()
+    #return HttpResponse(f"{resp}")
+    #return render(request, "business/connexion.html")
+
+#class HiddenAdminLoginView(LoginView):
+def adminLogin(request):
+    #template_name = 'admin_login.html'
+    f = open("C:/xampp/htdocs/ECF/SiteWeb/ZEvent/ZEvent/business/templates/business/admin_login.html", "r")
     resp = f.read()
     return HttpResponse(f"{resp}")
+
+
+class ConnexionView(TemplateView):
+    template_name = 'business/connexion.html'
